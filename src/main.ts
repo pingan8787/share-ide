a5a9ad5913e8b817a9b7234963bc0ffb613b24df
wangpingan <pingan8787@qq.com>
2022-03-27 00:02:04 +0800

    feat: 移除element-plus/dist/index.css

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import ElementPlus from 'element-plus';
-import 'element-plus/dist/index.css';
+// import 'element-plus/dist/index.css';
 import draggable from 'vuedraggable';
 import hljsVuePlugin from '@highlightjs/vue-plugin';
 
